42ee11b4d4c7e782850d59f26706cb77055bcad6
l3miage-pichouda <Adrien.Pichoud@etu.univ-grenoble-alpes.fr>
2024-04-30 15:55:42 +0200

    30/04 15:55

--- server/src/main/java/fr/uga/l3miage/integrator/components/CamionLoader.java
+++ server/src/main/java/fr/uga/l3miage/integrator/components/CamionLoader.java
@@ -2,10 +2,8 @@ package fr.uga.l3miage.integrator.components;
 
 import fr.uga.l3miage.integrator.dataType.GeoPosition;
 import fr.uga.l3miage.integrator.models.CamionEntity;
-import fr.uga.l3miage.integrator.models.EmployeEntity;
-import fr.uga.l3miage.integrator.models.enums.Emploi;
 import fr.uga.l3miage.integrator.repositories.CamionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,12 +13,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
+@AllArgsConstructor
 public class CamionLoader implements CommandLineRunner {
-    @Autowired
-    private CamionRepository camionRepository;
+
+    private final CamionRepository camionRepository;
     @Override
     public void run(String... args) throws Exception{
-        Path path = Path.of("server/src/main/resources/Export_Camions_netoye.csv");
+        Path path = Path.of("server/src/main/resources/Camions.csv");
         List<String> lines = Files.readAllLines(path);
         List<CamionEntity> camions = lines.stream().skip(1) // Skip header line
                 .map(line -> line.split(","))
