b01a347cd72877adcbc273738024cf3c11e6cad5
l3miage-osmanm <mona.osman@etu.univ-grenoble-alpes.fr>
2024-05-07 09:30:09 +0200

    modifReussiCmdGrpByClient

--- server/src/main/java/fr/uga/l3miage/integrator/services/CommandeService.java
+++ server/src/main/java/fr/uga/l3miage/integrator/services/CommandeService.java
@@ -8,6 +8,7 @@ import fr.uga.l3miage.integrator.models.ClientEntity;
 import fr.uga.l3miage.integrator.models.CommandeEntity;
 import fr.uga.l3miage.integrator.models.LivraisonEntity;
 import fr.uga.l3miage.integrator.responses.AdresseResponseDTO;
+import fr.uga.l3miage.integrator.responses.ClientCommandesPairResponseDTO;
 import fr.uga.l3miage.integrator.responses.ClientResponseDTO;
 import fr.uga.l3miage.integrator.responses.CommandeResponseDTO;
 import lombok.RequiredArgsConstructor;
@@ -52,24 +53,27 @@ public class CommandeService {
         }
     }
 
-    public Map<AdresseResponseDTO,Set<CommandeResponseDTO>> getCommandesGroupedByClient(){
+    public Set<ClientCommandesPairResponseDTO> getCommandesGroupedByClient(){
         try{
-            Map<Adresse, List<CommandeEntity>> commandesGroupedByClient = commandeComponent.getCommandesGroupedByClient();
+            try {
+                Set<CommandeComponent.ClientCommandesPair> commandesGroupedByClient = commandeComponent.getCommandesGroupedByClient();
 
-            Map<AdresseResponseDTO, Set<CommandeResponseDTO>> commandesDTOGroupedByClient = commandesGroupedByClient.entrySet().stream()
-                    .collect(Collectors.toMap(
-                            entry -> adresseMapper.toResponse(entry.getKey()),
-                            entry -> entry.getValue().stream()
-                                    .map(commandeMapper::toResponse)
-                                    .collect(Collectors.toSet())
-                    ));
-
-            return commandesDTOGroupedByClient;
-        }
-        catch (Exception e){
-            throw new RuntimeException(e);
+                return commandesGroupedByClient.stream()
+                        .map(pair -> new ClientCommandesPairResponseDTO(
+                                adresseMapper.toResponse(pair.getAdresse()),
+                                pair.getCommandes().stream()
+                                        .map(commandeMapper::toResponse)
+                                        .collect(Collectors.toSet())
+                        ))
+                        .collect(Collectors.toSet());
+            } catch (Exception e) {
+                throw new RuntimeException("commande service cmdGrpedByclient 1"+e);
+            }
+        }catch (Exception e){
+            throw new RuntimeException("commande service cmdGrpedByclient 2"+e);
         }
     }
+
     public CommandeResponseDTO updateEtat(String ref, String etat){
         return commandeMapper.toResponse(commandeComponent.updateEtat(ref, etat));
          }
