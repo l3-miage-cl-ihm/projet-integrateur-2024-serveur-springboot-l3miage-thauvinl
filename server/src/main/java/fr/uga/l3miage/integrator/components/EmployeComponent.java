b4d4a277287e35cb16545dc23842b21dda67e4a6
rahmanii <imen.rahmanii@univ-grenoble-alpes.fr>
2024-05-13 09:44:13 +0200

    gestion des exeptions

--- server/src/main/java/fr/uga/l3miage/integrator/components/EmployeComponent.java
+++ server/src/main/java/fr/uga/l3miage/integrator/components/EmployeComponent.java
@@ -21,20 +21,17 @@ import java.util.stream.Collectors;
 public class EmployeComponent {
     private final EmployeRepository employeRepository;
     private final TourneeRepository tourneeRepository;
-    
+
     public Set<EmployeEntity> getLivreursByTourneeId(String tourneeId) throws NotFoundTourneeEntityException {
         Optional<TourneeEntity> tourneeOptional = tourneeRepository.findById(tourneeId);
-        if (tourneeOptional.isPresent()) {
-            TourneeEntity tournee = tourneeOptional.get();
+        return tourneeOptional.map(tournee -> {
             // Filtrer les employés pour ne garder que ceux qui sont des livreurs
             return tournee.getEmployeEntitySet().stream()
                     .filter(employe -> employe.getEmploi() == Emploi.livreur)
                     .collect(Collectors.toSet());
-        } else {
-            // Gérer le cas où la tournée n'existe pas
-            throw new NotFoundTourneeEntityException("Tournée non trouvée avec l'ID : " + tourneeId);
-        }
+        }).orElseThrow(() -> new NotFoundTourneeEntityException("Tournée non trouvée avec l'ID : " + tourneeId));
     }
+
 
     public List<EmployeEntity> getAllEmployes() {
             return employeRepository.findAll().stream().collect(Collectors.toList());
@@ -46,36 +43,17 @@ public class EmployeComponent {
     }
 
     public EmployeEntity updateEmploye(String id, EmployeEntity employe) throws NotFoundEmployeEntityException {
-        Optional<EmployeEntity> optionalEmploye = employeRepository.findById(id);
-        if (optionalEmploye.isPresent()) {
-            EmployeEntity existingEmploye = optionalEmploye.get();
-            // Mettre à jour les attributs de l'employé existant avec les valeurs de l'employé fourni
-            existingEmploye.setEmail(employe.getEmail());
-            existingEmploye.setNom(employe.getNom());
-            existingEmploye.setPrenom(employe.getPrenom());
-            existingEmploye.setTelephone(employe.getTelephone());
-            existingEmploye.setEmploi(employe.getEmploi());
-            // Enregistrer l'employé mis à jour
-            return employeRepository.save(existingEmploye);
-        } else {
-            // gérer le cas où l'employé avec l'ID donné n'existe pas
-            throw new NotFoundEmployeEntityException("Employe introuvable");
-        }
+        return employeRepository.findById(id).orElseThrow(()->new NotFoundEmployeEntityException(String.format("Employe introuvable")));
     }
 
 
     public EmployeEntity getLivreurByEmail(String email) throws NotFoundEmployeEntityException {
-        Optional<EmployeEntity> optionalEmploye = employeRepository.findByEmailAndEmploi(email, Emploi.livreur);
-        if (optionalEmploye.isPresent()) {
-            return optionalEmploye.get();
-        } else {
-            throw new NotFoundEmployeEntityException("Livreur non trouvé avec l'email: " + email);
-        }
+        return  employeRepository.findByEmailAndEmploi(email, Emploi.livreur).orElseThrow(()->new NotFoundEmployeEntityException(String.format("Livreur non trouvé avec l'email: " + email)));
+
     }
 
     public Set<EmployeEntity> getAllLivreurs() {
-
-            return employeRepository.findAllByEmploi(Emploi.livreur);
+        return employeRepository.findAllByEmploi(Emploi.livreur);
 
     }
 
