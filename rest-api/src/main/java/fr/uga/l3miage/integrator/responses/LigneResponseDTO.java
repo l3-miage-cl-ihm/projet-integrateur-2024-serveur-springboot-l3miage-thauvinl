ba566b3a5a03aa8dad632db4031293d50c298797
l3miage-osmanm <mona.osman@etu.univ-grenoble-alpes.fr>
2024-05-15 15:14:04 +0200

    ajout readme.txt avec numGroupe + Nom

--- rest-api/src/main/java/fr/uga/l3miage/integrator/responses/LigneResponseDTO.java
+++ rest-api/src/main/java/fr/uga/l3miage/integrator/responses/LigneResponseDTO.java
@@ -1,5 +1,6 @@
 package fr.uga.l3miage.integrator.responses;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +11,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LigneResponseDTO {
+    @Schema(description = "Référence de la ligne" , example = "l01")
     private String reference;
+    @Schema(description = "Rle produit" )
     private ProduitResponseDTO produit;
+    @Schema(description = "quantité du produit" , example = "11")
     private int quantite;
+    @Schema(description = "prix " , example = "30")
     private double montant;
+    @Schema(description = "est-ce que le client a choisit l'option montage ou non" , example = "true")
     private boolean optionDeMontage;
 }
